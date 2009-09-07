b0efac6b799da30d0c9912de930ec18e577892bb
Don Kelly <karfai@gmail.com>
2009-09-07 00:48:53 -0400

    In order to add the "waiting at stop" version of the Panel, implement the GUI elements and population from the Model as a State pattern.

--- fe.gtk.py
+++ fe.gtk.py
@@ -67,6 +67,7 @@ class Model(object):
     def __init__(self):
         self._store = schema.create_store()
         self._results = Results()
+        self._current_stop = None
 
     def _build_search(self, s):
         rv = None
@@ -80,23 +81,38 @@ class Model(object):
         
         return rv
 
-    # def _prune_stops(self, stops, offset):
-    #     n = schema.secs_elapsed_today()
-    #     for stop in stops:
-    #         for pu in stop.upcoming_pickups(offset):
-    #             m = (pu.arrival - n) / 60
-    #             print '%s (%i): %s' % (stop.label, stop.number, stop.name)
-    #             print "%s %s in %um (%s)" % (pu.trip.route.name, pu.trip.headsign, m, pu.arrival_s())
-
     def _show_stops(self, stops):
         self._results.clear()
         for stop in stops:
             self._results.show(stop.label, stop.number, stop.name)
+
+    def format_current_stop(self):
+        return '%s (%s/%i)' % (self._current_stop.name, self._current_stop.label, self._current_stop.number)
+
+    def set_current_stop(self, code):
+        self._current_stop = self._store.find(schema.Stop, schema.Stop.label == unicode(code)).one()
             
     def stop_search(self, s):
         srch = self._build_search(s)
         if srch:
             self._show_stops(self._store.find(schema.Stop, srch))
+
+    def _format_arrival(self, m):
+        fmt = (m < 0) and 'Should have arrived %s ago' or 'Arriving in %s'
+        ms = (m > 60) and '%i:%i' % (m / 60, m % 60) or '%i minutes' % m
+        return fmt % ms
+
+    def upcoming_pickups_at_current(self, offset):
+        n = schema.secs_elapsed_today()
+        self._results.clear()
+        for pu in self._current_stop.upcoming_pickups(offset):
+            m = (pu.arrival - n) / 60
+            s = 'now'
+            if m < 0:
+                s = self._format_arrival(m)
+            elif m > 0:
+                s = self._format_arrival(m)
+            self._results.show(pu.trip.route.name, pu.trip.headsign, s)
 
     def results(self):
         return self._results
@@ -107,8 +123,10 @@ class Model(object):
 class InfoLabel(gtk.Label):
     def __init__(self, text):
         gtk.Label.__init__(self, None)
-        self.set_markup('<span size="x-large">%s</span>' % text)
         self.set_property('xalign', 0.0)
+
+    def change_text(self, text):
+        self.set_markup('<span size="x-large">%s</span>' % text)
         
 class FindButton(gtk.Button):
     def __init__(self, panel):
@@ -118,13 +136,13 @@ class FindButton(gtk.Button):
     def act_click(self, w, panel):
         panel.model().stop_search(panel.get_query_text())
 
-class SelectButton(gtk.Button):
+class NextStateButton(gtk.Button):
     def __init__(self, panel):
         gtk.Button.__init__(self, None, 'gtk-execute')
         self.connect('clicked', self.act_click, panel)
 
     def act_click(self, w, panel):
-        pass
+        panel.next()
 
 class LocationEntry(gtk.ComboBoxEntry):
     def __init__(self):
@@ -146,15 +164,66 @@ class ResultsTreeView(gtk.TreeView):
         for i in range(0, self.get_model().get_n_columns()):
             self.append_column(gtk.TreeViewColumn('', gtk.CellRendererText(), markup=i))
 
+class State(object):
+    def __init__(self, panel):
+        self._panel = panel
+
+    def start(self):
+        pass
+
+    def finish(self):
+        pass
+
+    def get_visibility(self):
+        return (True, True)
+
+    def get_info_text(self):
+        return ''
+
+    def model(self):
+        return self._panel.model()
+
+    def panel(self):
+        return self._panel
+
+    def next(self):
+        self.finish()
+        rv = self.next_class()(self._panel)
+        rv.start()
+        return rv
+
+class Wait(State):
+    def get_visibility(self):
+        return (True, False)
+
+    def start(self):
+        self.panel().model().upcoming_pickups_at_current(300)
+
+    def get_info_text(self):
+        return 'Waiting at %s' % self.panel().model().format_current_stop()
+
+class SelectStop(State):
+    def get_info_text(self):
+        return 'Where are you now?'
+
+    def finish(self):
+        r = self.panel().selected_result()
+        self.panel().model().set_current_stop(r[0])
+
+    def next_class(self):
+        return Wait
+
 class Panel(gtk.Window):
     def __init__(self):
         gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
 
-        self._model = Model()
-
         self.set_border_width(6)
-        self._build_contents()
         self.connect('destroy', self.act_quit)
+
+        self._model = Model()
+        self._state = SelectStop(self)
+
+        self._build_contents()
 
     def _build_list(self):
         self._list = ResultsTreeView(self._model.results())
@@ -167,24 +236,25 @@ class Panel(gtk.Window):
         return sw
 
     def _build_contents(self):
-        self._location_entry = LocationEntry()
-
         vb = gtk.VBox(False, 4)
-        vb.pack_start(InfoLabel('Where are you now?'), False, True, 0)
 
-        hb = gtk.HBox(False, 8)
-        hb.pack_start(self._location_entry, True, True, 0)
+        self._info = InfoLabel('')
+        vb.pack_start(self._info, False, True, 0)
 
-        hb2 = gtk.HBox(False, 2)
-        hb2.pack_start(FindButton(self), True, True, 0)
-        hb2.pack_start(gtk.Button('GPS'), True, True, 0)
+        self._search_box = gtk.HBox(False, 8)
 
-        hb.pack_start(hb2, True, True, 0)
+        self._location_entry = LocationEntry()
+        self._search_box.pack_start(self._location_entry, True, True, 0)
 
-        vb.pack_start(hb, False, True, 0)
+        hb = gtk.HBox(False, 2)
+        hb.pack_start(FindButton(self), True, True, 0)
+        hb.pack_start(gtk.Button('GPS'), True, True, 0)
+        self._search_box.pack_start(hb, True, True, 0)
+
+        vb.pack_start(self._search_box, False, True, 0)
         vb.pack_start(self._build_list(), True, True, 0)
 
-        vb.pack_start(SelectButton(self), False, True, 0)
+        vb.pack_start(NextStateButton(self), False, True, 0)
 
         self.add(vb)
 
@@ -197,6 +267,36 @@ class Panel(gtk.Window):
     def model(self):
         return self._model
 
+    def next(self):
+        self._state = self._state.next()
+        self.refresh()
+
+    def refresh(self):
+        self.show_all()
+
+        self._info.change_text(self._state.get_info_text())
+
+        vis = self._state.get_visibility()
+        if vis[0]:
+            self._info.show_all()
+        else:
+            self._info.hide_all()
+        if vis[1]:
+            self._search_box.show_all()
+        else:
+            self._search_box.hide_all()
+
+    def selected_result(self):
+        sel = self._list.get_selection()
+        rv = None
+        if sel:
+            (m, it) = sel.get_selected()
+            if it:
+                rv = []
+                [rv.append(m.get_value(it, i)) for i in range(0, m.get_n_columns())]
+        return rv
+
+
 w = Panel()
-w.show_all()
+w.refresh()
 gtk.main()
